7c2c3bfc3a6077628e7200e38db7e7c5627e5fbe
Johan Lundgren <johan.emil.lundgren@gmail.com>
2016-03-30 15:13:42 +0200

    first commit

---
+++ components/app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import React, {
+    Component,
+    Text,
+    View
+} from 'react-native';
+
+
+export default class App extends Component {    
+    render(){
+        return (
+            <Text>HEJ HOPP</Text>
+        );
+    }
+}
