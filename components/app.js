774e86915ea0161d379ab61627ab82e833e070fc
Johan Lundgren <johan.emil.lundgren@gmail.com>
2016-03-30 16:16:33 +0200

    fixed listView from fetched json

--- components/app.js
+++ components/app.js
@@ -3,14 +3,86 @@
 import React, {
     Component,
     Text,
+    Image,
+    StyleSheet,
+    ListView,
     View
 } from 'react-native';
 
+let URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
-export default class App extends Component {    
+export default class App extends Component {
+    constructor(){
+        super();
+        var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.state = {
+            dataSource: new ListView.DataSource({
+                rowHasChanged: (row1, row2) => row1 !== row2,
+            }),
+        };
+    }
+    componentDidMount(){
+        fetch(URL)
+        .then((response) => response.json())
+        .then((responseData) => {
+            this.setState({
+                dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+            });
+        })
+        .done();
+    }
+    renderMovie(movie) {
+        return (
+        <View style={styles.container}>
+            <Image
+                source={{uri: movie.posters.thumbnail}}
+                style={styles.thumbnail}
+            />
+            <View style={styles.rightContainer}>
+            <Text style={styles.title}>{movie.title}</Text>
+            <Text style={styles.year}>{movie.year}</Text>
+            </View>
+        </View>
+        );
+    }
     render(){
         return (
-            <Text>HEJ HOPP</Text>
+            <ListView
+                dataSource={this.state.dataSource}
+                renderRow={this.renderMovie}
+                style={styles.listView}
+            />
         );
     }
 }
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    borderColor: 'black',
+    borderBottomWidth: 1,
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 18,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
+  listView: {
+    marginTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+});
